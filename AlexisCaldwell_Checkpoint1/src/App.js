f06c63aed493214cf1823b868a85ad8ee226d17c
acaldwell1997 <46656008+acaldwell1997@users.noreply.github.com>
2019-08-21 22:04:53 -0500

    Add files via upload

---
+++ AlexisCaldwell_Checkpoint1/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './App.css';
+import Header from './components/header/Header';
+import Footer from './components/footer/Footer';
+import Main from './components/Main/Main';
+import { BrowserRouter as Router } from 'react-router-dom'
+
+function App() {
+  return (
+    <Router>
+		<div className="container">
+			<Header />
+			<Main />
+			<Footer /> 
+		</div>
+	</Router>
+  ); 
+}
+
+export default App;
